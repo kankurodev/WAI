f1362b079dbf25d2b2bb08d2ef34d99da0f5521f
Toby Horn <kankuro865@gmail.com>
2018-06-10 00:37:37 -0500

    Removed duplicated files and fixed incorrect routes.

--- raffle/resources/views/layouts/app.blade.php
+++ raffle/resources/views/layouts/app.blade.php
@@ -1,8 +1,8 @@
-@extends('head')
+@extends('layouts.head')
 @section('title', 'Raffler')
 <body class="container bg-darker">
     <div id="app">
-        @include('header')
+        @include('layouts.header')
 
         <main class="py-4">
             @yield('content')
--- raffle/resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Welcome | Raffler')
-@section('content')
-    <div class="container text-light">
-        <h2>Welcome!</h2>
-    </div>
-@endsection
